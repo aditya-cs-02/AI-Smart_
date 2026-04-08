81b821bc16e2656455c46d4f9c39bee78a249e2c
Aditya Sharma <aditya02.cs@gmail.com>
2026-04-08 22:28:49 +0530

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,14 +1,6 @@
-import React from 'react'
-import { StrictMode } from 'react' // Import StrictMode specifically
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import React{ useState, useEffect } from 'react'
-import { supabase } from './supabaseClient.js' // Added .js
-import Auth from './Auth.jsx'                  // Added .jsx
-import Dashboard from './Dashboard.jsx'        // Added .jsx
-
-// If you have a global CSS file, import it here
-// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
