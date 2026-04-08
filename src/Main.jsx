a437a0252d49659f0290cb5ac724f1e9a34696ef
Aditya Sharma <aditya02.cs@gmail.com>
2026-04-08 21:31:18 +0530

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient.js' // Added .js
+import Auth from './Auth.jsx'                  // Added .jsx
+import Dashboard from './Dashboard.jsx'        // Added .jsx
 
 // If you have a global CSS file, import it here
 // import './index.css' 
